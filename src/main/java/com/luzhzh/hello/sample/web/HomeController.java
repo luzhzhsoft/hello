52a835e5d897f6beed3faf037fb71b0253760dcb
luzhzh <luzhzh@bw30.com>
2019-04-10 18:37:48 +0800

    1111

---
+++ src/main/java/com/luzhzh/hello/sample/web/HomeController.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.luzhzh.hello.sample.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author jitendra on 5/3/16.
+ */
+@RestController
+public class HomeController {
+
+	@RequestMapping("/setValue")
+	public String setValue(@RequestParam(name = "key", required = false) String key,
+			@RequestParam(name = "value", required = false) String value,
+			HttpServletRequest request) {
+		if (!ObjectUtils.isEmpty(key) && !ObjectUtils.isEmpty(value)) {
+			request.getSession().setAttribute(key, value);
+		}
+		return "home";
+	}
+
+/*	@RequestMapping(value = "/")
+	public Map<String,String> helloUser(Principal principal){
+		HashMap<String,String> result=new HashMap<>();
+		result.put("username",principal.getName());
+		return result;
+	}*/
+
+
+	@RequestMapping(value = "/login")
+	public String login(@RequestParam(name = "username")  String user,@RequestParam(name = "password") String password){
+		if(user.equals("admin")&&password.equals("123456")){
+			BlockingQueue
+			return "success";
+		}else {
+			return "failed";
+		}
+	}
+
+
+	@RequestMapping("/logout")
+	public void logout(HttpSession httpSession){
+		httpSession.invalidate();
+	}
+}
